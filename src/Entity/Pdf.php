f3f9bd35926ce29876547184eb8eee6eecf64c72
Vincent Mazurais <fhezrghjsd4@live.fr>
2023-10-05 08:32:20 +0200

    pdf

--- src/Entity/Pdf.php
+++ src/Entity/Pdf.php
@@ -2,8 +2,9 @@
 
 namespace App\Entity;
 
-use App\Repository\PdfRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\PdfRepository;
 
 #[ORM\Entity(repositoryClass: PdfRepository::class)]
 class Pdf
