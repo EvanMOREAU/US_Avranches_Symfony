785562cbf7347a41cbea906770069be6e414b3a6
Evan Moreau <moreeva@ndlp.fr>
2023-10-11 12:01:58 +0200

    Patch

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -28,12 +28,14 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
-
+            $user->setRoles([
+                "ROLE_PLAYER",
+            ]);
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_default');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('registration/register.html.twig', [
