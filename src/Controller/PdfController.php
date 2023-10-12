83a1e0dd0ab0bc272d5a24f1565672acc8195569
Tristan Lefaist <lefatri@ndlp.fr>
2023-10-12 11:55:06 +0200

    patch pdf et test telecharger stats

--- src/Controller/PdfController.php
+++ src/Controller/PdfController.php
@@ -6,78 +6,97 @@ use TCPDF;
 use App\Entity\Pdf;
 use App\Entity\User;
 use App\Repository\UserRepository;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 class PdfController extends AbstractController
- {
-    #[ Route( '/pdf', name: 'app_pdf' ) ]
-
-    public function pdf( UserRepository $userRepository ): Response
+{
+    #[Route('/pdf', name: 'app_pdf')]
+    public function pdf(UserRepository $userRepository): Response
     {
+        // Récupérez le token d'authentification de l'utilisateur actuellement connecté.
         $token = $this->get('security.token_storage')->getToken();
 
-            $pdf = new Pdf();
-            if ($token instanceof TokenInterface) {
-                $user = $token->getUser();
+        // Créez une nouvelle instance de la classe PDF.
+        $pdf = new Pdf();
 
-                if ($user instanceof User) {
+        if ($token instanceof TokenInterface) {
+            // Récupérez l'utilisateur à partir du token d'authentification.
+            $user = $token->getUser();
 
+            if ($user instanceof User) {
+                // Configuration du PDF
+                $pdf->SetAuthor('SIO TEAM ! 💻');
+                $pdf->SetTitle('Fiche joueur');
+                $pdf->SetFont('times', '', 14);
 
-                    $pdf->SetAuthor( 'SIO TEAM ! 💻' );
-                    $pdf->SetTitle( 'Fiche joueur' );
-                    $pdf->SetFont( 'times', '', 14 );
+                // Ajout d'une nouvelle page
+                $pdf->AddPage();
+                $pdf->setJPEGQuality(75);
 
-                    $pdf->AddPage();
-                    $pdf->setJPEGQuality( 75 );
+                // Calcul des dimensions de la page
+                $largeurPage = $pdf->getPageWidth() + 30;
+                $hauteurPage = $pdf->getPageHeight() - 25;
 
-                    $largeurPage = $pdf->getPageWidth() + 30;
-                    $hauteurPage = $pdf->getPageHeight()- 25;
+                // Configuration de la police et des couleurs
+                $pdf->SetFont('helvetica', '', 20);
+                $pdf->SetTextColor(0, 0, 0);
 
-                    $pdf->SetFont('helvetica', '', 20);
-                
-                    $pdf->SetTextColor(0,0,0);
-                    $pdf->MultiCell(80, 10, "FICHE DU JOUEUR", 0, '', 0, 1, '', '', false, 0, false, false, 0, '');
-                    $pdf->MultiCell(70, 10, $user->getFirstName().' '.$user->getLastName().' ('.$user->getId(). ')', 0, 'C', 0, 1, '', '', true);
-                    
-                    $pdf->SetFont('helvetica', 'B', 20);
-                    $pdf->SetTextColor(255,255,255);
+                // Ajout du titre de la fiche du joueur
+                $pdf->MultiCell(80, 10, "FICHE DU JOUEUR", 0, '', 0, 1, '', '', false, 0, false, false, 0, '');
 
-                    $pdf->SetTextColor(255,255,255);
+                // Ajout du nom du joueur
+                $pdf->MultiCell(70, 10, $user->getFirstName() . ' ' . $user->getLastName() . ' (' . $user->getId() . ')', 0, 'C', 0, 1, '', '', true);
 
-                    $textg = '
-                    <style>.link { color: rgb(42,56,114); }</style>
-                    <br><br><br><br>
-                    <b><i>Informations concernant le joueur : </i></b>
-                    <br><br>
-                    <p><b>Date d\'anniversaire : </b>'. $user->getDateNaissance()->format('d/m/Y') .'
-                    <br><hr><br><div></div>
-                    <b>Catégorie</b> : '. $user->getCategory().'
-                    <br><hr><br><div></div>
-                    <b>Contact :</b>
-                    </p><p>
-                    <br> Christelle DELARUE<br>
-                    <br>
-                    Club House US Avranches MSM<br>
-                    Allée Jacques Anquetil<br>
-                    50300 Avranches.<br><br>
-                    Tel : 02.33.48.30.78 <br><br>
-                    Mail : <span class="link"><u>communication@us-avranches.fr</u></span><br>
-                    Mail : <span class="link"><u>partenaires@us-avranches.fr</u></span><br>
-                    Mail : <span class="link"><u>us.avranches@orange.fr</u></span>
-                    </p>';
+                // Configuration de la police et des couleurs pour le contenu du joueur
+                $pdf->SetFont('helvetica', 'B', 20);
+                $pdf->SetTextColor(255, 255, 255);
 
+                // Contenu du joueur (avec HTML)
+                $textg = '
+                <style>.link { color: rgb(42, 56, 114) }</style>
+                <br><br><br><br>
+                <b><i>Informations concernant le joueur : </i></b>
+                <br><br>
+                <p><b>Date de naissance : </b>' . $user->getDateNaissance()->format('d/m/Y') . '
+                <br><hr><br><div></div>
+                <b>Catégorie : </b>U' . $user->getCategory() . '
+                <br><hr><br><div></div>
+                Nombre de matchs joués :
+                <br><hr><br><div></div>
+                </p>
 
-                    $pdf->SetFont( 'helvetica', '', 10 );
-                    $pdf->writeHTMLCell( 65, 230, '', '', $textg, 0, 0, 0, true, '', true );
+                <p><b> Contact :</b>
+                <br> Christelle DELARUE<br>
+                <br>
+                Club House US Avranches MSM<br>
+                Allée Jacques Anquetil<br>
+                50300 Avranches.<br><br>
+                <b>Téléphone</b> : 02.33.48.30.78 <br><br>
+                <b>Mails</b> :<br>
+                <span class="link"><u>communication@us-avranches.fr</u></span><br>
+                <span class="link"><u>partenaires@us-avranches.fr</u></span><br>
+                <span class="link"><u>us.avranches@orange.fr</u></span>
+                </p>';
 
-                    $pdf->Image('img/anonyme.jpg', 130, 33.3, 40, 45, '', '', '', false, 300, '', false, false, 1, false, false, false);
+                // Ajout du contenu du joueur au PDF
+                $pdf->SetFont('helvetica', '', 10);
+                $pdf->writeHTMLCell(65, 230, '', '', $textg, 0, 0, 0, true, '', true);
 
-                    return $pdf->Output( 'US-Avranches-' . '.pdf', 'I' );
+                // Ajout d'une image au PDF
+                $pdf->Image('img/chartTest.jpg', 95, 150, 100, 100, '', '', '', false, 300, '', false, false, 1, false, false, false);
+
+                // Ajout d'une image au PDF
+                $pdf->Image('img/anonyme.jpg', 130, 33.3, 40, 45, '', '', '', false, 300, '', false, false, 1, false, false, false);
+
+                // Génération du PDF et envoi en réponse
+                return $pdf->Output('US-Avranches-' . '.pdf', 'I');
             }
         }
+
+        // Gestion des cas d'erreur
+        return new Response('Erreur');
     }
 }
