1c188df9c6f75465da0d6016f769e448ed66a4d9
Vincent Mazurais <fhezrghjsd4@live.fr>
2023-10-19 08:46:29 +0200

    1

--- migrations/Version20231019062510.php
+++ migrations/Version20231019062510.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231009140046 extends AbstractMigration
+final class Version20231019062510 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,7 @@ final class Version20231009140046 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE pdf (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, content LONGTEXT DEFAULT NULL, stats INT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE tbl_category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE tbl_player (id INT AUTO_INCREMENT NOT NULL, team_id INT NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, birthdate DATE NOT NULL, matches_played INT NOT NULL, INDEX IDX_15BF13DA296CD8AE (team_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE tbl_team (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, matches_played INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE tbl_user (id INT AUTO_INCREMENT NOT NULL, team_id INT DEFAULT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, date_naissance DATE NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, matches_played INT NOT NULL, UNIQUE INDEX UNIQ_38B383A1F85E0677 (username), INDEX IDX_38B383A1296CD8AE (team_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -34,7 +34,7 @@ final class Version20231009140046 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE tbl_player DROP FOREIGN KEY FK_15BF13DA296CD8AE');
         $this->addSql('ALTER TABLE tbl_user DROP FOREIGN KEY FK_38B383A1296CD8AE');
-        $this->addSql('DROP TABLE pdf');
+        $this->addSql('DROP TABLE tbl_category');
         $this->addSql('DROP TABLE tbl_player');
         $this->addSql('DROP TABLE tbl_team');
         $this->addSql('DROP TABLE tbl_user');
---
+++ src/Controller/PdfController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Controller;
+
+use TCPDF;
+use App\Entity\Pdf;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+class PdfController extends AbstractController
+{
+    #[Route('/pdf', name: 'app_pdf')]
+    public function pdf(UserRepository $userRepository): Response
+    {
+        // Récupérez le token d'authentification de l'utilisateur actuellement connecté.
+        $token = $this->get('security.token_storage')->getToken();
+
+        // Créez une nouvelle instance de la classe PDF.
+        $pdf = new Pdf();
+
+        if ($token instanceof TokenInterface) {
+            // Récupérez l'utilisateur à partir du token d'authentification.
+            $user = $token->getUser();
+
+            if ($user instanceof User) {
+                // Configuration du PDF
+                $pdf->SetAuthor('SIO TEAM ! 💻');
+                $pdf->SetTitle('Fiche joueur');
+                $pdf->SetFont('times', '', 14);
+
+                // Ajout d'une nouvelle page
+                $pdf->AddPage();
+                $pdf->setJPEGQuality(75);
+
+                // Calcul des dimensions de la page
+                $largeurPage = $pdf->getPageWidth() + 30;
+                $hauteurPage = $pdf->getPageHeight() - 25;
+
+                // Configuration de la police et des couleurs
+                $pdf->SetFont('helvetica', '', 20);
+                $pdf->SetTextColor(0, 0, 0);
+
+                // Ajout du titre de la fiche du joueur
+                $pdf->MultiCell(80, 10, "FICHE DU JOUEUR", 0, '', 0, 1, '', '', false, 0, false, false, 0, '');
+
+                // Ajout du nom du joueur
+                $pdf->MultiCell(70, 10, $user->getFirstName() . ' ' . $user->getLastName() . ' (' . $user->getId() . ')', 0, 'C', 0, 1, '', '', true);
+
+                // Configuration de la police et des couleurs pour le contenu du joueur
+                $pdf->SetFont('helvetica', 'B', 20);
+                $pdf->SetTextColor(0, 0, 0);
+
+                // Contenu du joueur (avec HTML)
+                $textg = '
+                <style>.link { color: rgb(42, 56, 114) }</style>
+                <br><br><br><br>
+                <b><i>Informations concernant le joueur : </i></b>
+                <br><br>
+                <p><b>Date de naissance : </b>' . $user->getDateNaissance()->format('d/m/Y') . '
+                <br><hr><br><div></div>
+                <b>Catégorie : </b>' . $user->getCategory() . '
+                <br><hr><br><div></div>
+                <b>Nombre de matchs joués :</b>
+                <br><hr><br><div></div>
+                <b>Poids : </b>
+                <br><hr><br><div></div>
+                <b>Taille : </b>
+                <br><hr><br><div></div>
+                </p>
+
+                <p><b> Contact :</b>
+                <br> Christelle DELARUE<br>
+                <br>
+                Club House US Avranches MSM<br>
+                Allée Jacques Anquetil<br>
+                50300 Avranches.<br><br>
+                <b>Téléphone</b> : 02.33.48.30.78 <br><br>
+                <b>Mails</b> :<br>
+                <span class="link"><u>communication@us-avranches.fr</u></span><br>
+                <span class="link"><u>partenaires@us-avranches.fr</u></span><br>
+                <span class="link"><u>us.avranches@orange.fr</u></span>
+                </p>';
+
+                // Ajout du contenu du joueur au PDF
+                $pdf->SetFont('helvetica', '', 10);
+                $pdf->writeHTMLCell(65, 230, '', '', $textg, 0, 0, 0, true, '', true);
+
+                // Ajout d'une image au PDF
+                $pdf->Image('img/graph_'. $user->getFirstName() .'.jpg', 95, 150, 100, 100, '', '', '', false, 300, '', false, false, 1, false, false, false);
+
+                // Ajout d'une image au PDF
+                $pdf->Image('img/anonyme.jpg', 130, 33.3, 40, 45, '', '', '', false, 300, '', false, false, 1, false, false, false);
+
+                // Génération du PDF et envoi en réponse
+                return $pdf->Output('US-Avranches-' . '.pdf', 'I');
+            }
+        }
+
+        // Gestion des cas d'erreur
+        return new Response('Erreur');
+    }
+
+    #[Route('/pdftest', name: 'app_pdftest')]
+    public function pdfTest(): Response
+    {
+        $user = $this->getUser(); // Récupérez l'utilisateur actuellement connecté
+
+        return $this->render('/pdf/index.html.twig', [
+            'controller_name' => 'DefaultController',
+            'user' => $user, // Assurez-vous que 'user' est correctement défini
+        ]);
+    }
+}
