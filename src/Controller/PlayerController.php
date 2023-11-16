2e8bff46f5c932c6521975ac8f75c0a0c17f3e82
Alexandre Pain <painale@ndlp.fr>
2023-11-16 11:00:33 +0100

    bd

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -4,16 +4,51 @@ namespace App\Controller;
 
 use App\Entity\Player;
 use App\Form\PlayerType;
+use Psr\Log\LoggerInterface;
 use App\Repository\PlayerRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/player')]
 class PlayerController extends AbstractController
 {
+    #[Route('/poste/set-poste-principal/{id}', name: 'app_set_poste_principal')]
+    public function setPostePrincipal(Player $player, Request $request, LoggerInterface $logger): Response
+    {
+        $logger->debug('setPostePrincipal() player->getFirstname() = ' . $player->getFirstname());
+
+        // Récupérez les données de la requête AJAX
+        $postePrincipal = $_POST["postePrincipal"];
+        $logger->debug('setPostePrincipal() postePrincipal = ' . $postePrincipal);
+
+        // Mettez à jour l'entité Player
+        $player->setPostePrincipal($postePrincipal);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($player);
+        $entityManager->flush();
+    }
+
+    #[Route('/poste/set-poste-secondaire/{id}', name: 'app_set_poste_secondaire')]
+    public function setPosteSecondaire(Player $player, Request $request, LoggerInterface $logger): Response
+    {
+        $logger->debug('setPosteSecondaire() player->getFirstname() = ' . $player->getFirstname());
+
+        // Récupérez les données de la requête AJAX
+        $posteSecondaire = $_POST["posteSecondaire"];
+        $logger->debug('setPosteSecondaire() posteSecondaire = ' . $posteSecondaire);
+
+        // Mettez à jour l'entité Player
+        $player->setPosteSecondaire($posteSecondaire);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($player);
+        $entityManager->flush();
+    }
+
     #[Route('/', name: 'app_player_index', methods: ['GET'])]
     public function index(PlayerRepository $playerRepository): Response
     {
@@ -67,7 +102,7 @@ class PlayerController extends AbstractController
             'form' => $form,
         ]);
     }
-
+    
     #[Route('/{id}', name: 'app_player_delete', methods: ['POST'])]
     public function delete(Request $request, Player $player, EntityManagerInterface $entityManager): Response
     {
@@ -80,10 +115,15 @@ class PlayerController extends AbstractController
     }
 
     #[Route('/{id}/poste', name: 'app_player_poste', methods: ['GET'])]
-    public function poste(Player $player): Response
+    public function poste(Player $player, LoggerInterface $logger): Response
     {
+        // $logger->debug('poste() player->getFirstname() = ' . $player->getFirstname());
         return $this->render('player/poste.html.twig', [
             'player' => $player,
         ]);
     }
+
+    //////////////////////////////
+
+
 }
