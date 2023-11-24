d9e604d8b3162bf562763a11774745d23db84910
Vincent Mazurais <fhezrghjsd4@live.fr>
2023-11-24 14:30:47 +0100

    patch

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -16,7 +16,7 @@ class CategoryFixtures extends Fixture
         for ($i = 10; $i <= 13; $i++) {
             $category = new Category();
             $category->setName("U" . $i);
-            $category->setImage("0");
+            $category->setImage('0');
             $manager->persist($category);
         }
 
