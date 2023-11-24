1bae3a4d86ec7c0e0b6dcfc6c93b3b69d34c61b1
Evan Moreau <moreeva@ndlp.fr>
2023-11-24 14:38:22 +0100

    patch_presentation

--- migrations/Version20231124133708.php
+++ migrations/Version20231124133708.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231123093338 extends AbstractMigration
+final class Version20231124133708 extends AbstractMigration
 {
     public function getDescription(): string
     {
