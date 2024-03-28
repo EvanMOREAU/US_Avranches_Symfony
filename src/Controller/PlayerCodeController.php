4013d85f874dd7c0a503b45460936a04c5eacc2d
Evan Moreau <evan.moreau@etik.com>
2024-03-28 08:41:21 +0100

    patch categorie (Evan)

--- src/Controller/PlayerCodeController.php
+++ src/Controller/PlayerCodeController.php
@@ -31,44 +31,7 @@ class PlayerCodeController extends AbstractController
 
         return $this->render('player_code/index.html.twig', [
             'player_codes' => $playerCodeRepository->findAll(),
-            'location' => '',
-        ]);
-    }
-
-    #[Route('/new', name: 'app_player_code_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        if(!$this->userVerificationService->verifyUser()){
-            return $this->redirectToRoute('app_verif_code', [], Response::HTTP_SEE_OTHER);
-        }
-
-        $playerCode = new PlayerCode();
-        $form = $this->createForm(PlayerCodeType::class, $playerCode);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($playerCode);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_player_code_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('player_code/new.html.twig', [
-            'player_code' => $playerCode,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_player_code_show', methods: ['GET'])]
-    public function show(PlayerCode $playerCode): Response
-    {
-        if(!$this->userVerificationService->verifyUser()){
-            return $this->redirectToRoute('app_verif_code', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('player_code/show.html.twig', [
-            'player_code' => $playerCode,
-            'location' => '',
+            'location' => 'm',
         ]);
     }
 
@@ -92,20 +55,5 @@ class PlayerCodeController extends AbstractController
             'player_code' => $playerCode,
             'form' => $form,
         ]);
-    }
-
-    #[Route('/{id}', name: 'app_player_code_delete', methods: ['POST'])]
-    public function delete(Request $request, PlayerCode $playerCode, EntityManagerInterface $entityManager): Response
-    {
-        if(!$this->userVerificationService->verifyUser()){
-            return $this->redirectToRoute('app_verif_code', [], Response::HTTP_SEE_OTHER);
-        }
-
-        if ($this->isCsrfTokenValid('delete'.$playerCode->getId(), $request->request->get('_token'))) {
-            $entityManager->remove($playerCode);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('app_player_code_index', [], Response::HTTP_SEE_OTHER);
     }
 }
